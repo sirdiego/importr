944f818e1146ae2dcea4d4755b08177db1868781
Tim Lochmüller <tim.lochmueller@hdnet.de>
2016-06-09 17:08:23 +0200

    [TASK] Migration to Twitter Boostrap

--- ext_emconf.php
+++ ext_emconf.php
@@ -21,7 +21,7 @@ $EM_CONF[$_EXTKEY] = [
     'author_company' => '',
     'constraints' => [
         'depends' => [
-            'typo3' => '6.2.0-7.6.99',
+            'typo3' => '7.0.0-8.1.99',
             'php' => '5.5.0-0.0.0'
         ],
     ],
