69e9025ed6bf2aa5a21e62210492ab0edcb9496f
Christian Lewin <christian.lewin@hdnet.de>
2016-07-08 15:31:41 +0200

    Add support for the new method of registering wizards

--- Configuration/TCA/tx_importr_domain_model_import.php
+++ Configuration/TCA/tx_importr_domain_model_import.php
@@ -5,137 +5,143 @@ if (!defined('TYPO3_MODE')) {
 }
 
 return [
-    'ctrl' => [
-        'title' => 'LLL:EXT:importr/Resources/Private/Language/locallang.xlf:tx_importr_domain_model_import',
-        'label' => 'starttime',
-        'label_alt' => 'filepath',
+    'ctrl'      => [
+        'title'           => 'LLL:EXT:importr/Resources/Private/Language/locallang.xlf:tx_importr_domain_model_import',
+        'label'           => 'starttime',
+        'label_alt'       => 'filepath',
         'label_alt_force' => 1,
-        'searchFields' => 'filepath',
-        'rootLevel' => 1,
-        'default_sortby' => 'ORDER BY starttime',
-        'delete' => 'deleted',
-        'iconfile' => 'EXT:importr/Resources/Public/Icons/Import.png',
+        'searchFields'    => 'filepath',
+        'rootLevel'       => 1,
+        'default_sortby'  => 'ORDER BY starttime',
+        'delete'          => 'deleted',
+        'iconfile'        => 'EXT:importr/Resources/Public/Icons/Import.png',
     ],
     'interface' => [
         'showRecordFieldList' => 'strategy,filepath,starttime,endtime,pointer,amount',
     ],
-    'types' => [
+    'types'     => [
         '1' => ['showitem' => 'strategy,filepath,starttime,endtime,pointer,amount'],
     ],
-    'columns' => [
+    'columns'   => [
         'starttime' => [
-            'label' => 'LLL:EXT:lang/locallang_general.xlf:LGL.starttime',
+            'label'  => 'LLL:EXT:lang/locallang_general.xlf:LGL.starttime',
             'config' => [
-                'type' => 'input',
-                'size' => 13,
-                'max' => 20,
-                'eval' => 'datetime',
+                'type'     => 'input',
+                'size'     => 13,
+                'max'      => 20,
+                'eval'     => 'datetime',
                 'checkbox' => 0,
-                'default' => 0,
-                'range' => [
+                'default'  => 0,
+                'range'    => [
                     'lower' => mktime(0, 0, 0, date('m'), date('d'), date('Y'))
                 ],
             ],
         ],
-        'endtime' => [
-            'label' => 'LLL:EXT:lang/locallang_general.xlf:LGL.endtime',
+        'endtime'   => [
+            'label'  => 'LLL:EXT:lang/locallang_general.xlf:LGL.endtime',
             'config' => [
-                'type' => 'input',
-                'size' => 13,
-                'max' => 20,
-                'eval' => 'datetime',
+                'type'     => 'input',
+                'size'     => 13,
+                'max'      => 20,
+                'eval'     => 'datetime',
                 'checkbox' => 0,
-                'default' => 0,
-                'range' => [
+                'default'  => 0,
+                'range'    => [
                     'lower' => mktime(0, 0, 0, date('m'), date('d'), date('Y'))
                 ],
             ],
         ],
-        'pointer' => [
-            'label' => 'LLL:EXT:importr/Resources/Private/Language/locallang.xlf:pointer',
+        'pointer'   => [
+            'label'  => 'LLL:EXT:importr/Resources/Private/Language/locallang.xlf:pointer',
             'config' => [
                 'type' => 'none',
                 'size' => 30,
                 'eval' => 'trim'
             ],
         ],
-        'amount' => [
-            'label' => 'LLL:EXT:importr/Resources/Private/Language/locallang.xlf:amount',
+        'amount'    => [
+            'label'  => 'LLL:EXT:importr/Resources/Private/Language/locallang.xlf:amount',
             'config' => [
                 'type' => 'none',
                 'size' => 30,
                 'eval' => 'trim'
             ],
         ],
-        'errors' => [
-            'label' => 'LLL:EXT:importr/Resources/Private/Language/locallang.xlf:error',
+        'errors'    => [
+            'label'  => 'LLL:EXT:importr/Resources/Private/Language/locallang.xlf:error',
             'config' => [
                 'type' => 'none',
                 'size' => 30,
                 'eval' => 'trim'
             ],
         ],
-        'inserted' => [
-            'label' => 'LLL:EXT:importr/Resources/Private/Language/locallang.xlf:error',
+        'inserted'  => [
+            'label'  => 'LLL:EXT:importr/Resources/Private/Language/locallang.xlf:error',
             'config' => [
                 'type' => 'none',
                 'size' => 30,
                 'eval' => 'trim'
             ],
         ],
-        'updated' => [
-            'label' => 'LLL:EXT:importr/Resources/Private/Language/locallang.xlf:error',
+        'updated'   => [
+            'label'  => 'LLL:EXT:importr/Resources/Private/Language/locallang.xlf:error',
             'config' => [
                 'type' => 'none',
                 'size' => 30,
                 'eval' => 'trim'
             ],
         ],
-        'ignored' => [
-            'label' => 'LLL:EXT:importr/Resources/Private/Language/locallang.xlf:error',
+        'ignored'   => [
+            'label'  => 'LLL:EXT:importr/Resources/Private/Language/locallang.xlf:error',
             'config' => [
                 'type' => 'none',
                 'size' => 30,
                 'eval' => 'trim'
             ],
         ],
-        'unknowns' => [
-            'label' => 'LLL:EXT:importr/Resources/Private/Language/locallang.xlf:error',
+        'unknowns'  => [
+            'label'  => 'LLL:EXT:importr/Resources/Private/Language/locallang.xlf:error',
             'config' => [
                 'type' => 'none',
                 'size' => 30,
                 'eval' => 'trim'
             ],
         ],
-        'filepath' => [
-            'label' => 'LLL:EXT:importr/Resources/Private/Language/locallang.xlf:filepath',
+        'filepath'  => [
+            'label'  => 'LLL:EXT:importr/Resources/Private/Language/locallang.xlf:filepath',
             'config' => [
-                'type' => 'input',
-                'size' => 50,
-                'eval' => 'trim,required',
+                'type'    => 'input',
+                'size'    => 50,
+                'eval'    => 'trim,required',
                 'wizards' => [
                     '_PADDING' => 2,
-                    'link' => [
-                        'type' => 'popup',
-                        'title' => 'Link',
-                        'icon' => 'link_popup.gif',
-                        'script' => 'browse_links.php?mode=wizard',
+                    'link'     => [
+                        'type'         => 'popup',
+                        'title'        => 'Link',
+                        'icon'         => 'link_popup.gif',
+                        'module'       => [
+                            'name'          => 'wizard_element_browser',
+                            'urlParameters' => [
+                                'mode' => 'wizard',
+                                'act'  => 'file'
+                            ]
+                        ],
                         'JSopenParams' => 'height=300,width=500,status=0,menubar=0,scrollbars=1',
-                        'params' => [
-                            'blindLinkOptions' => 'page,url,mail,spec,folder',
+                        'params'       => [
+                            'blindLinkOptions'  => 'page,url,mail,spec,folder',
                             'allowedExtensions' => $GLOBALS['TYPO3_CONF_VARS']['SYS']['textfile_ext'],
                         ]
                     ],
                 ],
             ],
         ],
-        'strategy' => [
-            'label' => 'LLL:EXT:importr/Resources/Private/Language/locallang.xlf:strategy',
+        'strategy'  => [
+            'label'  => 'LLL:EXT:importr/Resources/Private/Language/locallang.xlf:strategy',
             'config' => [
-                'type' => 'select',
+                'type'          => 'select',
                 'foreign_table' => 'tx_importr_domain_model_strategy',
-                'maxitems' => 1,
-                'size' => 1,
+                'maxitems'      => 1,
+                'size'          => 1,
             ],
         ],
     ],
